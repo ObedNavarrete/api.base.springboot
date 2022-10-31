21862ec3ff60f8bd87ed4cbace88ff900d072e4e
ObedNavarrete <84824423+ObedNavarrete@users.noreply.github.com>
2022-10-31 09:06:54 -0600

    Add methods utiles

--- src/main/java/project/base/security/util/UtilityBase.java
+++ src/main/java/project/base/security/util/UtilityBase.java
@@ -68,4 +68,63 @@ public class UtilityBase {
         calendar.add(Calendar.DAY_OF_YEAR, days);
         return calendar.getTime();
     }
+    
+    // AddMonthsToDate
+    public static Date addMonthsToDate(Date date, int months){
+        if (months==0) return date;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH, months);
+        return calendar.getTime();
+    }
+
+    // Verificar si el usuario logueado tiene el rol
+    public Map<String, Boolean> hasRole(String role) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Map<String, Boolean> response = new java.util.HashMap<>();
+        response.put("hasRole", authentication.getAuthorities().stream().anyMatch(r -> r.getAuthority().equals(role)));
+        return response;
+    }
+
+    // Verificar si el usuario tiene el rol
+    // Ejenplo de uso => Boolean result = this.idUserHasRole(1,"ROLE_CUSTOMER");
+    public Boolean idUserHasRole(Integer id, String rol) {
+        Boolean response = false;
+        Users user = usersRepository.findByPasiveIsFalseAndId(id);
+        if (user == null) {
+            return false;
+        }
+        response = user.getRoles().stream().anyMatch(r -> r.getName().equals(rol));
+        return response;
+    }
+
+    // Verificar si el usuario tiene el uno de los roles
+    // Ejenplo de uso => Boolean result = this.idUserHasAnyRole(1, new String[]{"ROLE_CUSTOMER", "ROLE_MANAGER"});
+    public Boolean idUserHasAnyRole(Integer id, String[] roles) {
+        Boolean response = false;
+        Users user = usersRepository.findByPasiveIsFalseAndId(id);
+        if (user == null) {
+            return false;
+        }
+        for (String rol : roles) {
+            response = user.getRoles().stream().anyMatch(r -> r.getName().equals(rol));
+            if (response) {
+                break;
+            }
+        }
+        return response;
+    }
+
+    // Verificar si el usuario sigue siendo válido
+    public boolean isUserValid() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Map<String, Boolean> response = new java.util.HashMap<>();
+
+        Integer id = usersRepository.findIdLogued(authentication.getName());
+        if (id == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
